437a7c445cdb61e1d50e936e6a425726d874b76a
thangtd <thangtd@pavana.tech>
2025-09-25 11:28:12 +0700

    first_commit

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    titleWeb: "Mong em sẽ giữ mãi nụ cười <3",
+    introTitle: 'Tên người bạn đang nghĩ đến trong đầu ',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'Example: Tô Đức Thắng',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/ucthang.165710/'
